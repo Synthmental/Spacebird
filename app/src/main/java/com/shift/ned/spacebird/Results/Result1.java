b4ac03600e739109e3e6193bbdf61af0b7898530
synthmental@gmail.com <25802580majestic007>
2017-05-09 13:18:29 +0300

    commit

---
+++ app/src/main/java/com/shift/ned/spacebird/Results/Result1.java
@@ -0,0 +1,15 @@
+package com.shift.ned.spacebird.Results;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shift.ned.spacebird.R;
+
+public class Result1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result1);
+    }
+}
---
+++ app/src/main/java/com/shift/ned/spacebird/TestActivities/Test19.java
@@ -0,0 +1,15 @@
+package com.shift.ned.spacebird.TestActivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shift.ned.spacebird.R;
+
+public class Test19 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test19);
+    }
+}
