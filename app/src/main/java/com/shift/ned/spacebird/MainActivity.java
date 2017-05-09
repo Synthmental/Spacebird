7d66941512f6065957a7815ca0f635f8043a6113
synthmental@gmail.com <25802580majestic007>
2017-05-09 14:13:18 +0300

    commit

--- app/src/main/java/com/shift/ned/spacebird/MainActivity.java
+++ app/src/main/java/com/shift/ned/spacebird/MainActivity.java
@@ -1,12 +1,18 @@
 package com.shift.ned.spacebird;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.shift.ned.spacebird.TestActivities.Test1;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     private AdView mAdView;
 
@@ -18,5 +24,14 @@ public class MainActivity extends AppCompatActivity {
         mAdView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Test1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/shift/ned/spacebird/TestActivities/Test1.java
+++ app/src/main/java/com/shift/ned/spacebird/TestActivities/Test1.java
@@ -3,13 +3,21 @@ package com.shift.ned.spacebird.TestActivities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.shift.ned.spacebird.R;
 
 public class Test1 extends AppCompatActivity {
+
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test1);
+
+        mAdView = (AdView) findViewById(R.id.adView1);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 }
